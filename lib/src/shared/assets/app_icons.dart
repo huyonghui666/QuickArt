cf45b86761f054631e6c45d44051f8e35301133a
huyonghui <2795916525@qq.com>
2025-11-11 16:35:09 +0800

    修改首页顶部的icon

---
+++ lib/src/shared/assets/app_icons.dart
@@ -0,0 +1,14 @@
+class AppIcons {
+  AppIcons._();
+
+  static const String _svgBasePath = 'assets/icons/svg';
+
+  //首页的icon--------------------------------------
+  //首页顶部的icon
+  static const String homeTitle = '$_svgBasePath/Home/home_ic_title.svg';
+  static const String homeVip = '$_svgBasePath/Home/home_ic_vip.svg';
+  static const String homeSettings = '$_svgBasePath/Home/btn_home_settings.svg';
+
+
+
+}

5f3ca554ed8c16fd73e336ed1e18393841ac4b48
huyonghui <2795916525@qq.com>
2026-01-31 15:18:25 +0800

    修改项目架构

--- lib/src/core/exception/exception.dart
+++
@@ -1,8 +0,0 @@
-class AppException implements Exception {
-  final String message;
-  AppException(this.message);
-}
-
-class NetworkException extends AppException {
-  NetworkException(super.message);
-}
--- lib/src/features/quick_art/home/domain/repositories/text_to_image_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:async';
-
-import 'package:quick_art/src/features/quick_art/home/data/models/image_generation_task_model.dart';
-
-abstract class TextToImageRepository {
-  Future<ImageGenerationTaskModel> generateImage(String prompt);
-}
